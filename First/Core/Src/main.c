6a61ea1bad236dd77bc60b91c2de2927b0e3bd6d
VladimirDjokovic <vladaspec@gmail.com>
2025-06-28 22:01:05 +0200

    Linker script - First setup

--- First/Core/Src/main.c
+++ First/Core/Src/main.c
@@ -62,7 +62,7 @@ static void MX_USB_PCD_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
+__attribute__((section(".my_code")))
 /* USER CODE END 0 */
 
 /**
@@ -176,7 +176,7 @@ static void MX_I2C1_Init(void)
 
   /* USER CODE END I2C1_Init 1 */
   hi2c1.Instance = I2C1;
-  hi2c1.Init.Timing = 0x00201D2B;
+  hi2c1.Init.Timing = 0x2000090E;
   hi2c1.Init.OwnAddress1 = 0;
   hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
   hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
